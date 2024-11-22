186dccac4607d3e02487be4813506c6d9621575a
HasinMahir77 <hasinmahir@gmail.com>
2024-11-23 05:56:10 +0600

    host 0.0.0.0

--- backend/traffic/app.py
+++ backend/traffic/app.py
@@ -136,7 +136,6 @@ def changeSequence(deviceId):
     except Exception as e:
         return jsonify({"error": f"An unexpected error occurred: {str(e)}"}), 500
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run(debug=True, host='0.0.0.0')
 
 
