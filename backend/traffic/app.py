ba9f98fe43af73ae03c40bb692a2d759e3c64878
HasinMahir77 <hasinmahir@gmail.com>
2024-11-24 11:46:10 +0600

    setMode added

--- backend/traffic/app.py
+++ backend/traffic/app.py
@@ -177,6 +177,26 @@ def setManualColor(deviceId):
         return jsonify({"error": "Error decoding the device list file"}), 500
     except Exception as e:
         return jsonify({"error": f"An unexpected error occurred: {str(e)}"}), 500
+
+@app.route('/setMode/<deviceId>', methods=['POST'])
+def setMode(deviceId):
+    try:
+        with open(deviceListPath, 'r') as file:
+            device_list = json.load(file)
+        if deviceId not in device_list:
+            return jsonify({"error": "Device doesn't exist"}), 400
+        #Changes here
+        data = request.get_json()
+        device_list[deviceId]["mode"] = data["mode"]
+        with open(deviceListPath, 'w') as file:
+            json.dump(device_list, file, indent=4)
+
+    except FileNotFoundError:
+        return jsonify({"error": "Device list file not found"}), 500
+    except json.JSONDecodeError:
+        return jsonify({"error": "Error decoding the device list file"}), 500
+    except Exception as e:
+        return jsonify({"error": f"An unexpected error occurred: {str(e)}"}), 500
     
 if __name__ == '__main__':
         app.run(debug=False, threaded=True, host='0.0.0.0')
