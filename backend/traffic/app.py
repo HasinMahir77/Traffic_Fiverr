1924e223fb20c6be9cab5fc82943ddd765e3bf17
HasinMahir77 <hasinmahir@gmail.com>
2024-11-24 09:12:22 +0600

    Update app.py

--- backend/traffic/app.py
+++ backend/traffic/app.py
@@ -45,6 +45,19 @@ def getDevice(deviceId):
         return jsonify(device)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    
+@app.route('/getSequence/<deviceId>', methods=['GET'])
+def getDevice(deviceId):
+    print(f"Received request for sequence: {deviceId}")
+    try:
+        with open(sequenceListPath, 'r') as file:
+            sequence = json.load(file).get(deviceId, None)
+            if sequence is None:
+                return jsonify({"error": "Sequence not found"}), 404
+        return jsonify(sequence)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 
 @app.route('/getSequence/<deviceId>', methods=['GET'])
 def getSequence(deviceId):
@@ -133,6 +146,27 @@ def changeSequence(deviceId):
         return jsonify({"error": "Sequence list file not found"}), 500
     except json.JSONDecodeError:
         return jsonify({"error": "Error decoding the sequence list file"}), 500
+    except Exception as e:
+        return jsonify({"error": f"An unexpected error occurred: {str(e)}"}), 500
+
+@app.route('/setState/<deviceId>', methods=['POST'])
+def setState(deviceId):
+    try:
+        with open(deviceListPath, 'r') as file:
+            device_list = json.load(file)
+        if deviceId not in device_list:
+            return jsonify({"error": "Device doesn't exist"}), 400
+        #Changes here
+        device_list[deviceId]["color"]
+        with open(deviceListPath, 'w') as file:
+            json.dump(device_list, file, indent=4)
+
+        return jsonify({"message": f"Device {deviceId} removed successfully"}), 200
+
+    except FileNotFoundError:
+        return jsonify({"error": "Device list file not found"}), 500
+    except json.JSONDecodeError:
+        return jsonify({"error": "Error decoding the device list file"}), 500
     except Exception as e:
         return jsonify({"error": f"An unexpected error occurred: {str(e)}"}), 500
 if __name__ == '__main__':
